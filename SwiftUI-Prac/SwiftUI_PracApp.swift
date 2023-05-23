d0e0845a0b6aa2a8c07fab8a591221131bf62c34
mattc <tingyugenius@gmail.com>
2023-05-23 17:07:46 +0800

    Initial Commit

---
+++ SwiftUI-Prac/SwiftUI_PracApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracApp.swift
+//  SwiftUI-Prac
+//
+//  Created by matt on 2023/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
